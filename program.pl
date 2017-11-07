132fbeb8f2efd469ace088a59a293c289c10f318
ivanrgx <ivanrgx@gmail.com>
2017-11-07 15:25:08 -0600

    Lists and how to access them

--- program.pl
+++ program.pl
@@ -1,14 +1,8 @@
 #!/usr/bin/perl
-# arraylen.plx
+# badprefix.plx
 use warnings;
 use strict;
-my @array1;
-my $scalar1;
-@array1 = qw(Monday Tuesday Wednesday Thursday Friday Saturday Sunday);
-$scalar1 = @array1;
-print "Array 1 is @array1\nScalar 1 is $scalar1\n";
-my @array2;
-my $scalar2;
-@array2 = qw(Winter Spring Summer Autumn);
-$scalar2 = @array2;
-print "Array 2 is @array2\nScalar 2 is $scalar2\n";
+my @array = (1, 3, 5, 7, 9);
+my $scal;
+$scal = (@array)[-1];
+print $scal;
