1c4073fde58f77d5b9fbde3e79cb6615d0984d68
ivanrgx <ivanrgx@gmail.com>
2017-11-16 20:21:30 -0600

    Numeros primos - for

--- program.pl
+++ program.pl
@@ -1,12 +1,30 @@
 #!/usr/bin/perl
-# next.plx
-use strict;
+# looploop3.plx
 use warnings;
-my @array = (8, 3, 0, 2, 12, 0);
-for my $x(@array) {
-if ($x == 0) {
-print "Skipping zero element.\n";
-next;
-}
-print "48 over $x is ", 48/$x, "\n";
-}
+use strict;
+my @getout = qw(usd mxn eur);
+my $primo=0;
+
+
+print "Enter number\n";
+OUTER: while (<STDIN>) {
+      chomp;
+      OUTER2: for my $var(2..$_){
+          print ("Checando numero $var\n");
+          for my $x(1 .. $var){
+                 if($var%$x==0){
+                    $primo++;
+                 } 
+             }
+          if($primo!=2){
+          }
+          else{
+              print("Numero primo! $var\n");
+          }
+          $primo=0;
+          last OUTER if $var == $_;
+          } 
+          #print "$var\n";
+      }
+
+
