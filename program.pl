68d5bbdece02c098b7d793b169249f1a37600e59
ivanrgx <ivanrgx@gmail.com>
2017-11-18 09:49:47 -0600

    RE

--- program.pl
+++ program.pl
@@ -1,31 +1,10 @@
 #Test!!
 #!/usr/bin/perl
 # looploop3.plx
-use warnings;
+#use warnings;
 use strict;
-my @getout = qw(usd mxn eur);
-my $primo=0;
 
-
-print "Enter number\n";
-OUTER: while (<STDIN>) {
-      chomp;
-      OUTER2: for my $var(2..$_){
-          print ("Checando numero $var\n");
-          for my $x(1 .. $var){
-                 if($var%$x==0){
-                    $primo++;
-                 } 
-             }
-          if($primo!=2){
-          }
-          else{
-              print("Numero primo! $var\n");
-          }
-          $primo=0;
-          last OUTER if $var == $_;
-          } 
-          #print "$var\n";
-      }
-
-
+my $_= "\.\.\.";
+if (/.../){
+	print "Se encontro la cadena 10\n";
+}
