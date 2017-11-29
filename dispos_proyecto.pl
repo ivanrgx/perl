cefe550a264395b72520ec1aa2331f45fd8f7ca7
ivanrgx <ivanrgx@gmail.com>
2017-11-29 14:47:51 -0600

    Proyecto dispos

---
+++ dispos_proyecto.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl -w
+use strict;
+
+# Proyecto final de dispositivos
+# Programa que regresa el sentido magnético teórico (+ o -) que deberá guardarse en un disco magnético
+# de una entrada específica en binario
+# El sentido magnético dependerá de la cadena de entrada y método de codificación
+
+# NOTA: Se tomará como sentido + el campo que precede a la parte del disco magnético donde se grabaría la información
+
+
+# Variable de entrada
+my $bin_data_in;
+print ("Ingresar dato de entrada\n");
+$bin_data_in = <STDIN>;
+chomp($bin_data_in);
+print("\n\n");
+
+#Se checa que el dato sea valido
+
+unless($bin_data_in=~ /^[01]+$/){ 
+	die "Dato de entrada invalido, unicamente ingresar 0's y 1's\n\n";
+}
+
+# Método de codificación: Non-Return-to-Zero-Inverted (NRZI)
+# simply encodes a 1 as a magnetic polarity transition, also known as a "flux reversal", and a zero as no transition.
+
+my $tmp="+";
+my $sentido;
+my @salida;
+my @bin_data_in = split(//,$bin_data_in);
+
+foreach my $char (@bin_data_in){
+	# Dependiendo de el bit de entrada, es que se determinará el sentido del campo magnético
+        # que se guardará en el disco magnético;
+	if($char eq "1" and $tmp eq "+"){
+		$sentido = "-";
+	} 
+	elsif($char eq "1" and $tmp eq "-"){
+		$sentido = "+";
+	}
+	elsif($char eq "0" and $tmp eq "+"){
+		$sentido = "+";
+	}
+	elsif($char eq "0" and $tmp eq "-"){
+		$sentido = "-";
+	}
+	
+	# Se guarda el sentido en el arreglo @salida
+	push (@salida, $sentido);
+
+	# Para determinar los cambios de polaridad (T o NT) se guarda el sentido de esta iteracion en $tmp
+	$tmp = $sentido;
+}
+
+
+# Imprime salida que se guardaria en el disco magnético
+
+print "\nDato entrada: $bin_data_in\n";
+foreach my $el (@salida){
+		print "$el";
+}
+print"\n";
